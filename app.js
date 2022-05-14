988080894589261b76a9ca110be728b3481861ef
James Pretorius <jamesmpretorius@gmail.com>
2022-05-14 13:14:44 -0400

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+logger.info('Connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => logger.info('Connected to MongoDB'))
+  .catch(() => logger.error('Error connecting to MongoDB'))
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs/', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
