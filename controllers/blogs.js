6f5bd851728321108b8494402540e7a7ebb24008
James Pretorius <jamesmpretorius@gmail.com>
2022-05-17 10:57:07 -0400

    blog put and delete

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -19,4 +19,22 @@ blogRouter.post('/', (request, response) => {
     })
 })
 
+blogRouter.delete('/:id', async (req, res) => {
+  const id = req.params.id
+
+  Blog.findByIdAndDelete(id)
+
+  console.log()
+
+  res.status(204).end()
+})
+
+blogRouter.put('/:id', async (req, res) => {
+  console.log(req.body)
+  const newBlog = req.body
+
+  const newBlogResponse = await Blog.findByIdAndUpdate(req.params.id, newBlog, { new: true })
+  res.status(204).end()
+})
+
 module.exports = blogRouter
