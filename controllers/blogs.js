759b6429dd624f8f48451051bbe48cac9c74ec31
James Pretorius <jamesmpretorius@gmail.com>
2022-05-24 14:17:40 -0400

    user auth to middleware

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,52 +1,28 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
-const jwt = require('jsonwebtoken')
-
-class InvalidTokenException extends Error {
-  constructor() {
-    super()
-    this.message = 'Invalid Token'
-    this.name = 'InvalidTokenException'
-  }
-}
-
-const decodeTokenFrom = (req) => {
-  const authorization = req.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    const token = authorization.substring(7)
-    return jwt.verify(token, process.env.SECRET)
-  } else {
-    return null
-  }
-}
+const middleware = require('../utils/middleware')
 
 blogRouter.get('/', async (request, response, next) => {
   const blogs = await Blog.find({}).populate('user', { username: true, name: true })
   response.json(blogs)
 })
 
-blogRouter.post('/', async (request, response, next) => {
+blogRouter.post('/', middleware.userExtractor, async (request, response, next) => {
   const body = request.body
-  const token = decodeTokenFrom(request)
-  if (!token || !token.id) {
-    next(new InvalidTokenException())
-    return
-  }
-  const userCreator = await User.findById(token.id)
 
   const blog = new Blog({
     content: body.content,
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: userCreator._id,
+    user: request.userId,
   })
 
   const savedBlog = await blog.save()
 
-  userCreator.blogs = userCreator.blogs.concat(savedBlog._id)
-  await userCreator.save()
+  request.user.blogs = request.user.blogs.concat(savedBlog._id)
+  await request.user.save()
 
   response.status(201).json(savedBlog)
 })
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,14 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+class InvalidTokenException extends Error {
+  constructor() {
+    super()
+    this.message = 'Invalid Token'
+    this.name = 'InvalidTokenException'
+  }
+}
 
 const requestLogger = (request, response, next) => {
   logger.info(request.method, request.path, request.body)
@@ -7,6 +17,31 @@ const requestLogger = (request, response, next) => {
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const userExtractor = async (req, res, next) => {
+  const authorization = req.get('authorization')
+
+  try {
+    if (!(authorization && authorization.toLowerCase().startsWith('bearer '))) {
+      throw (new InvalidTokenException())
+    }
+    const token = authorization.substring(7)
+
+    const tokenPayload = jwt.verify(token, process.env.SECRET)
+
+    if (!tokenPayload || !tokenPayload.id) {
+      throw (new InvalidTokenException())
+    }
+
+    req.userId = tokenPayload.id
+    req.user = await User.findById(tokenPayload.id)
+
+    next()
+  } catch (e) {
+    next(e)
+    return
+  }
 }
 
 const errorHandler = (error, request, response, next) => {
@@ -31,4 +66,5 @@ module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  userExtractor
 }
