121ec7970971f82234e366b891831f80d66513b2
James Pretorius <jamesmpretorius@gmail.com>
2022-08-04 17:34:14 -0400

    updated listener

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const http = require("http")
-const app = require("./lib/app")
-const logger = require("./lib/utils/logger")
-const config = require("./lib/utils/config")
+const app = require("./src/app")
+const logger = require("./src/utils/logger")
+const config = require("./src/utils/config")
 
-const server = http.createServer(app)
-
-server.listen(config.PORT, () => {
+app.listen(config.PORT, () => {
   logger.info(`Server started on port ${config.PORT}`)
 })
